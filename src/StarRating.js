26508ad6c2290556a29634ec5f7190124434e445
shubham4132 <shubhamg3122134@gmail.com>
2024-01-16 15:03:53 +0530

    propTypes checking maxrating prop we passed is number or string just like this

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -9,6 +10,15 @@ const containerStyle = {
 const starContainer = {
   display: "flex",
   gap: "4px",
+};
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  message: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 export default function StarRating({
   maxRating = 5,
