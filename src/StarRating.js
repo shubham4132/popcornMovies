815cc080bb4e0e67e093a05a15edfb9578eb8e99
shubham4132 <shubhamg3122134@gmail.com>
2024-01-16 14:44:18 +0530

    Improving Reusability with Props

--- src/StarRating.js
+++ src/StarRating.js
@@ -13,13 +13,17 @@ const starContainer = {
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
-  size = "48px",
+  size = 48,
+  message = [],
+  defaultRating = 0,
+  onSetRating,
 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
-
+  console.log(message.length);
   function handleRating(x) {
     setRating(x);
+    onSetRating(x);
   }
   const textStyle = {
     lineHeight: "1",
@@ -42,15 +46,20 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
-    height: `${size}`,
-    width: `${size}`,
+    height: `${size}px`,
+    width: `${size}px`,
     display: "block",
+    cursor: "pointer",
   };
   return (
     <span
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 //import "./index.css";
 //import App from "./App";
 import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />;
+      <p>This movie was Rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} color="red" />
+    <StarRating
+      maxRating={5}
+      size={24}
+      color="green"
+      message={["terrible", "bad", "okay", "good", "Excellent"]}
+    />
+    <StarRating maxRating={10} defaultRating={3} />
+    <StarRating maxRating={10} size={24} color="red" />
+    <Test />
   </React.StrictMode>
 );
