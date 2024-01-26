3353e8bdd951df1fb594da25130713d596bc709a
shubham4132 <shubhamg3122134@gmail.com>
2024-01-26 23:09:01 +0530

    Refs to Persists Data between the renders

--- src/App.js
+++ src/App.js
@@ -233,6 +233,17 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const countRef = useRef(0);
+  let count = 0;
+
+  useEffect(
+    function () {
+      if (userRating) countRef.current = countRef.current + 1;
+      if (userRating) count++;
+    },
+    [userRating]
+  );
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserRating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -271,6 +282,8 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
+      countDecision: count,
     };
 
     onAddWatched(newWatchedMovie);
